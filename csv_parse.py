db2baa03dc683d3067aa0871acb68a976b389a6d
Wayne Lu <wayne.lu.8@gmail.com>
2017-05-15 20:08:23 -0700

    Load train/dev labels

--- csv_parse.py
+++ csv_parse.py
@@ -5,12 +5,10 @@ def parse_labels(filepath):
     """
     Reads in the label CSV file at filepath.
 
-    Returns:
-        labels - an dictionary of speaker ID to L1 classification
-        languages - an array of L1 names
+    Returns: an dictionary of speaker ID to L1 classification
     """
 
-    languages = ["ARA", "CHI", "FRE", "GER", "HIN", "ITA", "JPN", "KOR", "SPA", "TEL", "TUR"]
+    languages = get_languages() 
 
     csvfile = open(filepath, 'r')
     reader = csv.reader(csvfile)
@@ -21,7 +19,20 @@ def parse_labels(filepath):
         labels[int(speaker_id)] = languages.index(L1)
     csvfile.close()
 
-    return labels, languages
+    return labels
+
+def get_train_labels():
+    train_path = "data/nli-shared-task-2017/data/labels/train/labels.train.csv"
+    return parse_labels(train_path) 
+
+def get_dev_labels():
+    dev_path = "data/nli-shared-task-2017/data/labels/dev/labels.dev.csv"
+    return parse_labels(dev_path)
+
+def get_languages():
+    return ["ARA", "CHI", "FRE", "GER", "HIN", "ITA", "JPN", "KOR", "SPA", "TEL", "TUR"]
 
 if __name__ == "__main__":
-    print parse_labels(sys.argv[1])
+    print "{} training labels".format(len(get_train_labels()))
+    print "{} dev labels".format(len(get_dev_labels()))
+    print "Languages: {}".format(" ".join(get_languages()))
